ee6049f97d1d2e333c97100b2a8cfb2a0c9e7d5a
Lau Llobet <lau.llobet@mypulse.ai>
2020-12-29 15:26:21 +0100

    🔴🔴🔴🔴 RED -  - to squash

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,4 +5,7 @@ describe('sample test-suite', () => {
     it("handle empty strings", () => {
         expect(wrap("", 3)).eql("");
     });
+    it("handle long words", () => {
+        expect(wrap("1234567890",5)).equal("12345\n67890");
+    })
 });
