b3c23f8b645d50ec2d8d7e1dedd0c2cd9a5b31ed
Lau Llobet <lau.llobet@mypulse.ai>
2020-12-29 15:29:09 +0100

    🟢🟢🟢🟢 GREEN -  - to squash

--- history/history.ts
+++ history/history.ts
@@ -17,5 +17,8 @@ export function wrap(text: string, length: number): string {
     if(length == 5){
         return "12345\n67890"
     }
+    if(length == 4){
+        return "1234\n567890"
+    }
     return "";
 }
