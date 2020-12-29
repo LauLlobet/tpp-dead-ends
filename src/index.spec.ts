9edd04762552aa92f0b378612070d79b957f923f
Lau Llobet <lau.llobet@mypulse.ai>
2020-12-29 15:33:54 +0100

    🔴🔴🔴🔴 RED -  - to squash

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -9,5 +9,7 @@ describe('sample test-suite', () => {
         expect(wrap("1234567890",5)).equal("12345\n67890");
         expect(wrap("1234567890",4)).equal("1234\n567890");
         expect(wrap("1234567890",3)).equal("123\n4567890");
+        expect(wrap("1234567890",2)).equal("12\n34567890");
+
     })
 });
