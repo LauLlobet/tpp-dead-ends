781e60ca5ba319a20123d2a438914905903d3db2
Lau Llobet <lau.llobet@mypulse.ai>
2020-12-29 15:15:49 +0100

    🟢🟢🟢🟢 GREEN -  - to squash

--- history/history.ts
+++ history/history.ts
@@ -1,14 +1,14 @@
-import { sum } from './index';
+import { wrap } from './index';
 import { expect } from 'chai';
 
 describe('sample test-suite', () => {
-    it('sum 1 + 3 should be 4', () => {
-        expect(sum(1, 3)).eql(4);
+    it("handle empty strings", () => {
+        expect(wrap("", 3)).eql("");
     });
 });
 
 //-------- IMPLEMENTATION --------
 
-export function sum(a: number, b: number): number {
-    return a + b;
+export function wrap(text: string, length: number): string {
+    return "";
 }
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,8 +1,8 @@
-import { sum } from './index';
+import { wrap } from './index';
 import { expect } from 'chai';
 
 describe('sample test-suite', () => {
-    it('sum 1 + 3 should be 4', () => {
-        expect(sum(1, 3)).eql(4);
+    it("handle empty strings", () => {
+        expect(wrap("", 3)).eql("");
     });
 });
