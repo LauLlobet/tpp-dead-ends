8f3be51d78a81a7799c04c620cebcc51f222a679
Lau Llobet <lau.llobet@mypulse.ai>
2020-12-29 16:15:19 +0100

    🔴🔴🔴🔴 RED -  - to squash

--- history/history.ts
+++ history/history.ts
@@ -5,6 +5,7 @@ describe('sample test-suite', () => {
     it('', () => {
         expect(wrap(3, undefined)).eql("");
         expect(wrap(3, "")).eql(""); //nil -> constant
+        expect(wrap(10, "word")).eql("word"); //constant -> constant+
     });
 });
 
