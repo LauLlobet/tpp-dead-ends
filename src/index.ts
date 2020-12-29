a0bb4b4079bc2213a7b5d307ce9b3a6e3167c5f9
Lau Llobet <lau.llobet@mypulse.ai>
2020-12-29 16:59:57 +0100

    🟢🟢🟢🟢 GREEN -  - to squash

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export function wrap(length: number, text: string): string {
     if(text == undefined){ // unconditional -> conditional
         return ""; // nil -> constant
     }
-    return text.replace(' ','\n') // statemet -> statements
+    return text.replace(/ /gi,'\n') // statemet -> statements
 }
