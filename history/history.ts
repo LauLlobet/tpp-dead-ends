28ede9498fcc26fed58d01d86f59700050fcbf61
Lau Llobet <lau.llobet@mypulse.ai>
2020-12-29 16:23:03 +0100

    🟢🟢🟢🟢 GREEN -  - to squash

--- history/history.ts
+++ history/history.ts
@@ -4,8 +4,11 @@ import { expect } from 'chai';
 describe('sample test-suite', () => {
     it('', () => {
         expect(wrap(3, undefined)).eql("");
+        expect(wrap(3, "")).eql(""); //nil -> constant
     });
 });
+
+
 
 //-------- IMPLEMENTATION --------
 
