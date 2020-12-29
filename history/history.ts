cd951c29702d12fba617c9acfaac334a42a1c1e7
Lau Llobet <lau.llobet@mypulse.ai>
2020-12-29 15:27:13 +0100

    🟢🟢🟢🟢 GREEN -  - to squash

--- history/history.ts
+++ history/history.ts
@@ -13,5 +13,8 @@ describe('sample test-suite', () => {
 //-------- IMPLEMENTATION --------
 
 export function wrap(text: string, length: number): string {
+    if(length == 5){
+        return "12345\n67890"
+    }
     return "";
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export function wrap(text: string, length: number): string {
+    if(length == 5){
+        return "12345\n67890"
+    }
     return "";
 }
