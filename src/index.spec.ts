de466043d0770b6a25582eaec316e62a9f111af6
Lau Llobet <lau.llobet@mypulse.ai>
2020-12-29 16:23:03 +0100

    🟢🟢🟢🟢 GREEN -  - to squash

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,5 +4,8 @@ import { expect } from 'chai';
 describe('sample test-suite', () => {
     it('', () => {
         expect(wrap(3, undefined)).eql("");
+        expect(wrap(3, "")).eql(""); //nil -> constant
     });
 });
+
+
